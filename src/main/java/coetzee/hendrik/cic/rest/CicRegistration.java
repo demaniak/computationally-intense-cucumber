4d0934e1bfc69ea17557ad9b923e0da8ab33b2f4
Hendrik Coetzee <hs.coetzee@gmail.com>
2018-02-08 00:16:16 +0200

    Coverage up to 62%

--- src/main/java/coetzee/hendrik/cic/rest/CicRegistration.java
+++ src/main/java/coetzee/hendrik/cic/rest/CicRegistration.java
@@ -4,9 +4,15 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import coetzee.hendrik.cic.entities.EntityEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CicRegistration {
     @NotNull
     @Size(min = 1, max = 128)    

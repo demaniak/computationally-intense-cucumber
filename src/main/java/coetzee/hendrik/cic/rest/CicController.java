298e92eeee89b666dca690e802a4344a3ed1d98c
Hendrik Coetzee <hs.coetzee@gmail.com>
2018-02-10 22:57:42 +0200

    Marginally better.
    At least I sorted out async rest controller tests.

--- src/main/java/coetzee/hendrik/cic/rest/CicController.java
+++ src/main/java/coetzee/hendrik/cic/rest/CicController.java
@@ -1,6 +1,6 @@
 /*
  * Project: cic
- * File:    CicController.java
+ * File: CicController.java
  * Created: Feb 7, 2018
  * 
  * Copyright (C) AfriGIS 2018
@@ -10,7 +10,10 @@ package coetzee.hendrik.cic.rest;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
+import java.net.URI;
 import java.util.concurrent.Callable;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,30 +21,38 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import coetzee.hendrik.cic.entities.CicEntity;
 import coetzee.hendrik.cic.services.CicService;
+import lombok.extern.slf4j.Slf4j;
 
 /**
- * Rest Controller for {@link CicEntity}'s 
+ * Rest Controller for {@link CicEntity}'s
  */
 @RestController
+@Slf4j
 public class CicController {
-    
+
     private CicService cicServ;
-    
+
     @Autowired
     public void setCicServ(CicService cicServ) {
         this.cicServ = cicServ;
     }
 
-    @RequestMapping(method=POST, path="/cic")
-    public Callable<ResponseEntity<CicEntity>> registerCic (@RequestBody CicRegistration nuCic) {
-        return () -> ResponseEntity.ok(cicServ.register (nuCic));
+    @RequestMapping(method = POST, path = "/cic")
+    public Callable<ResponseEntity<Object>> registerCic(@Valid @RequestBody CicRegistration nuCic) {
+        return () -> {            
+            final CicEntity result = cicServ.register(nuCic);
+            final URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{cidId}")
+                    .buildAndExpand(result.getCicId()).toUri();
+            return ResponseEntity.created(uri).build();
+        };
     }
-    
-    @RequestMapping (method = GET, path = "/cic/{cidId}")
-    public Callable<ResponseEntity<CicEntity>> get (@PathVariable Long cidId) {
-        return () -> ResponseEntity.ok(cicServ.get (cidId));
+
+    @RequestMapping(method = GET, path = "/cic/{cidId}")
+    public Callable<ResponseEntity<CicEntity>> get(@PathVariable Long cidId) {
+        return () -> ResponseEntity.ok(cicServ.get(cidId));
     }
 }
